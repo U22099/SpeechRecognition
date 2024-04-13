2e7422f19bb14c5ca092f2d3ba6b7dbdb74a8918
Daniel <nifemiolaniyi4@gmail.com>
2024-04-13 06:38:13 +0100

    ---

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: "/SpeechRecognition/",
-  plugins: [react()],
-  assetsInclude: ["**/*.{JPG,PNG,jpg,png}"],
-})
